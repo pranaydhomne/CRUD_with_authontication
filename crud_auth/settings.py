38286474a1d1198049bd4a558c536eece2455549
pranaydhomne <pranaydhomne9121@gmail.com>
2024-01-17 20:51:57 +0530

    git ignore file added

--- crud_auth/settings.py
+++ crud_auth/settings.py
@@ -153,12 +153,13 @@ MEDIA_URL = '/media/'
 
 
 #email settings 
+from decouple import config
 
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER='pranaydhomne2@gmail.com'
-EMAIL_HOST_PASSWORD='musqztdppatjprih'
+EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD') 
 
 
