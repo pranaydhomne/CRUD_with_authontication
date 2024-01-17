5bd0125f37ebd4cfdf2658a93495575db519cfe8
pranaydhomne <pranaydhomne9121@gmail.com>
2024-01-17 21:12:54 +0530

    default  value added in host pass

--- crud_auth/settings.py
+++ crud_auth/settings.py
@@ -160,6 +160,7 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER=config('EMAIL_HOST_USER') 
-EMAIL_HOST_PASSWORD= config('EMAIL_HOST_PASSWORD') 
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='your_default_password')
+
 
 
